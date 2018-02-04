88e639c996f4a33845db8a714098adff162eead6
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-02-04 17:54:18 +0300

    [Fixed #2] Sidebar component generated with mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <app-sidebar></app-sidebar>
-    <img src="./assets/logo.png">
-    <router-view/>
+    <main class="content">
+      <img src="./assets/logo.png">
+      <router-view/>
+    </main>
   </div>
 </template>
 
@@ -12,12 +14,4 @@ export default {
 }
 </script>
 
-<style lang="sass">
-  #app
-    font-family: 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    text-align: center
-    color: #2c3e50
-    margin-top: 60px
-</style>
+<style lang="sass" src="./styles/style.sass"></style>
