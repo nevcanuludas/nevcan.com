e29f777fcdf8179351536804e8f7f31a398157e5
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-07-07 10:11:22 +0300

    Fixed, Diving Activities in July 2018

--- src/components/ScheduledClasses/ScheduledClasses.vue
+++ src/components/ScheduledClasses/ScheduledClasses.vue
@@ -13,7 +13,10 @@ export default {
         {startDate: new Date('2018-04-30'), date: '30.04.2018', name: '1* Dalıcı Eğitimi', url: '/courses/1-yildiz-dalici', location: 'Enez', status: 'Dolu', class: ''},
         {startDate: new Date('2018-05-07'), date: '07.05.2018', name: 'Sidemount Uzmanlığı Eğitimi', url: 'courses/sidemount-uzmanligi', location: 'Sivas', status: 'Kontenjan (2/4)', class: ''},
         {startDate: new Date('2018-05-14'), date: '14.05.2018', name: 'Nitrox Uzmanlığı Eğitimi', url: '/courses/nitrox-uzmanligi', location: 'Antalya', status: 'Kontenjan (1/8)', class: ''},
-        {startDate: new Date('2018-05-15'), date: '15.05.2018', name: '1 ve 2 Yıldız Eğitmen Eğitimi', url: '/courses/', location: 'İstanbul', status: 'Dolu', class: ''}
+        {startDate: new Date('2018-05-15'), date: '15.05.2018', name: '1 ve 2 Yıldız Eğitmen Eğitimi', url: '/courses/', location: 'İstanbul', status: 'Dolu', class: ''},
+        {startDate: new Date('2018-07-14'), date: '14.07.2018', name: '1 Yıldız Dalıcı Eğitimi', url: '/courses/1-yildiz-dalici', location: 'İbrice Limanı', status: 'Dolu', class: ''},
+        {startDate: new Date('2018-07-21'), date: '21.07.2018', name: '1 Yıldız Dalıcı Eğitimi', url: '/courses/1-yildiz-dalici', location: 'İbrice Limanı', status: 'Dolu', class: ''},
+        {startDate: new Date('2018-07-28'), date: '28.07.2018', name: 'Serbest Dalış Eğitimi', url: '/courses/serbest-dalis-egitimi', location: 'İstanbul', status: 'Kontenjan (3/4)', class: ''}
       ]
     }
   },
