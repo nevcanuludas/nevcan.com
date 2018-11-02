84e5179ec7348d35bb37cf036f42c3c7ac0a55ae
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-02 22:55:11 +0300

    Fixed, dummy data added

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,10 +1,37 @@
+<i18n src="./Sidebar_locales.json"></i18n>
+<i18n>
+{
+  "en": {
+    "message": "English",
+    "title": "Scuba Instructor"
+  },
+  "tr": {
+    "message": "Türkçe",
+    "title": "Dalış Eğitmeni"
+  }
+}
+</i18n>
+
 <template lang="pug" src="./Sidebar.pug"></template>
 
 <script>
 export default {
   data () {
     return {
-      locale: 'tr'
+      locale: 'tr',
+      'en': {
+        'message': 'English',
+        'title': 'Scuba Instructor'
+      },
+      'tr': {
+        'message': 'Türkçe',
+        'title': 'Dalış Eğitmeni'
+      }
+    }
+  },
+  watch: {
+    locale (val) {
+      this.$i18n.locale = val
     }
   }
 }
