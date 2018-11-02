ccbcb94f1b453a3daf5585821837d88c9a0e7c36
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-02 09:50:06 +0300

    Fixed, services component structure

--- src/components/Services/Services.vue
+++ src/components/Services/Services.vue
@@ -5,7 +5,16 @@ export default {
   name: 'Services',
   data () {
     return {
-      sectionTitle: 'Verdiğim Hizmetler'
+      sectionTitle: 'Verdiğim Hizmetler',
+      services: [
+        { id: 'deneme-dalisi', imageUrl: 'dsd', name: 'Deneme Dalışı' },
+        { id: 'tazeleme-dalisi', imageUrl: 'refresh', name: 'Tazeleme Dalışı' },
+        { id: 'gelisim-dalisi', imageUrl: 'improvement', name: 'Gelişim Dalışı' },
+        { id: 'egitmen-hazirlik', imageUrl: 'beinginstructor', name: 'Eğitmenlik Kursuna Hazırlık' },
+        { id: 'gue-hazirlik', imageUrl: 'beinggue', name: 'GUE Kursuna Hazırlık' },
+        { id: 'fotograf-cekimi', imageUrl: 'photography', name: 'Sualtı Fotoğraf Çekimi' },
+        { id: 'kurumsal-danismanlik', imageUrl: 'consulting', name: 'Kurumsal Danışmanlık' }
+      ]
     }
   }
 }
