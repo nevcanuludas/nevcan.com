35cc0e0a783343f39857ead29d9fc00b4ab8e999
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-02 15:05:16 +0300

    Fixed, mapbox component added into contact component

---
+++ src/components/Mapbox/Mapbox.vue
@@ -0,0 +1,30 @@
+
+<template lang="pug" src="./Mapbox.pug"></template>
+
+<script>
+import VueMapboxMap from 'vue-mapbox-map'
+
+export default {
+  name: 'Mapbox',
+  data () {
+    return {
+      scene: {
+        accessToken: 'pk.eyJ1IjoibmV2Y2FudWx1ZGFzIiwiYSI6ImNqbnp1ejNocTBiMGwzdm1xNzVpejU0NG0ifQ.JJli50htD35CtZl-TW1twQ',
+        lng: 28.976420,
+        lat: 40.994360,
+        zoom: 9,
+        pitch: 20,
+        bearing: 0
+      }
+    }
+  },
+  methods: {
+    setMap (map) {
+      this.map = map
+    }
+  },
+  components: { VueMapboxMap }
+}
+</script>
+
+<style lang="sass" src="./Mapbox.sass" scoped></style>
