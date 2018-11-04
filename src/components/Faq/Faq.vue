960e170be67857d473b7ef969d6bda21e98c0db5
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-04 14:40:17 +0300

    [Fixed #42, service: Github] 🛠  "FAQ" component for general questions

--- src/components/Faq/Faq.vue
+++ src/components/Faq/Faq.vue
@@ -1,15 +1,21 @@
 <template lang="pug" src="./Faq.pug"></template>
 
 <script>
+import jsonFaq from './Faq.json'
+
 export default {
   name: 'Faq',
   data () {
     return {
-      sectionTitle: 'Sıkça Sorulan Sorular'
+      sectionTitle: 'Sıkça Sorulan Sorular',
+      faq: jsonFaq,
+      active_el: 0
     }
   },
   methods: {
-
+    activate (el) {
+      this.active_el = el
+    }
   }
 }
 </script>
