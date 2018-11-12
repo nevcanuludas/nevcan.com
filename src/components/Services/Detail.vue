b09cba64dcb4ee837ac8c7567a14af882cb52744
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-12 11:21:19 +0300

    Fixed, "verdiğim hizmetler" detay sayfaları router'a eklendi

---
+++ src/components/Services/Detail.vue
@@ -0,0 +1,37 @@
+<template lang='pug' src='./Detail.pug'></template>
+
+<script>
+import jsonServices from './Services.json'
+
+export default {
+  name: 'ServiceDetails',
+  data () {
+    return {
+      path: this.$route.params.id,
+      serviceTitle: '',
+      serviceScope: '',
+      serviceImages: '',
+      // JS Dictionary - Associative Arrays in JavaScript
+      dict: {
+        'deneme-dalisi': 0,
+        'tazeleme-dalisi': 1,
+        'gelisim-dalisi': 2,
+        'egitmen-hazirlik': 3,
+        'gue-hazirlik': 4,
+        'fotograf-cekimi': 5,
+        'kurumsal-danismanlik': 6
+      },
+      // Certificates JSON
+      services: jsonServices
+    }
+  },
+  created () {
+    var c = this.services[this.dict[this.path]]
+    this.serviceTitle = c.title
+    this.serviceScope = c.scope
+    this.serviceImages = c.images
+  }
+}
+</script>
+
+<style lang='sass' src='./Detail.sass' scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import CertificateDetails from '@/components/Certificates/Detail'
 import CourseDetails from '@/components/Courses/Detail'
+import ServiceDetails from '@/components/Services/Detail'
 import ConservationDetails from '@/components/Conservation/Detail'
 import ScheduledClasses from '@/components/ScheduledClasses/ScheduledClasses'
 import Faq from '@/components/Faq/Faq'
@@ -16,6 +17,7 @@ export default new Router({
     { path: '/', component: Home, name: 'Home' },
     { path: '/certificates/:id', component: CertificateDetails, name: 'CertificateDetails' },
     { path: '/courses/:id', component: CourseDetails, name: 'CourseDetails' },
+    { path: '/services/:id', component: ServiceDetails, name: 'ServiceDetails' },
     { path: '/conservation/:id', component: ConservationDetails, name: 'ConservationDetails' },
     { path: '/scheduled-classes', component: ScheduledClasses, name: 'ScheduledClasses' },
     { path: '/faq', component: Faq, name: 'Faq' },
