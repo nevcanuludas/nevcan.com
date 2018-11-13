c8d71a9bd76d38343c951149925fb682e7006f89
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-13 11:29:19 +0300

    Fixed, service detail page structure

--- src/components/Services/Detail.vue
+++ src/components/Services/Detail.vue
@@ -1,16 +1,24 @@
 <template lang='pug' src='./Detail.pug'></template>
 
 <script>
-import jsonServices from './Services.json'
+import jsonServiceDetails from './ServiceDetails.json'
 
 export default {
   name: 'ServiceDetails',
   data () {
     return {
       path: this.$route.params.id,
+      isVisible: false,
       serviceTitle: '',
-      serviceScope: '',
+      servicePrice: 'Hizmet bedeli belirtilmemiştir. Teklif almak için lütfen iletişime geçin.',
+      serviceNote: '',
       serviceImages: '',
+      serviceScope: '',
+      serviceHasPadiClass: false,
+      servicePrerequisites: '',
+      serviceIncludes: '',
+      serviceNotIncluded: 'Ulaşım, konaklama, yeme-içme ve kişisel harcamalar hizmet bedeline dahil değildir.',
+      serviceQuota: '',
       // JS Dictionary - Associative Arrays in JavaScript
       dict: {
         'deneme-dalisi': 0,
@@ -21,15 +29,26 @@ export default {
         'fotograf-cekimi': 5,
         'kurumsal-danismanlik': 6
       },
-      // Certificates JSON
-      services: jsonServices
+      // Service Details JSON
+      services: jsonServiceDetails
     }
   },
   created () {
-    var c = this.services[this.dict[this.path]]
-    this.serviceTitle = c.title
-    this.serviceScope = c.scope
-    this.serviceImages = c.images
+    var s = this.services[this.dict[this.path]]
+    this.serviceTitle = s.title
+    s.price && (this.servicePrice = s.price)
+    this.serviceNote = s.note
+    this.serviceScope = s.scope
+    this.serviceHasPadiClass = s.hasPadiClasses
+    this.serviceImages = s.images
+    this.servicePrerequisites = s.prerequisites
+    this.serviceIncludes = s.includes
+    this.serviceQuota = s.quota
+  },
+  methods: {
+    visible () {
+      this.isVisible = true
+    }
   }
 }
 </script>
