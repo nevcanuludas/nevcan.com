8e583dbeb5d7d67406455ccdb8482d00de950da1
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-11-22 20:02:08 +0300

    Refactor course details json data

--- src/components/Courses/Detail.vue
+++ src/components/Courses/Detail.vue
@@ -1,6 +1,10 @@
 <template lang='pug' src='./Detail.pug'></template>
 
 <script>
+import BirYildizDalici from './CourseDetailData/1-yildiz-dalici.json'
+import IkiYildizDalici from './CourseDetailData/2-yildiz-dalici.json'
+import UcYildizDalici from './CourseDetailData/3-yildiz-dalici.json'
+
 export default {
   name: 'CourseDetails',
   data () {
@@ -55,97 +59,9 @@ export default {
       },
       // Courses JSON
       courses: [
-        // 1* Dalıcı Kursu
-        {
-          title: '1* Dalıcı Kursu',
-          price: 'Kurs ücreti 250€´dur.',
-          scope: 'Kursun amacı başlangıç seviyesinde bir dalıcı yetirştirmektir. Temel teorik ve pratik eğitimleri alarak max 18 metreye kadar dalışlar gerçekleştirebilirler. Kurs sonunda isteğe bağlı olarak TSSF/CMAS veya ProTec belgesi alabilirsiniz. Alınan belgeler tüm dünyada geçerlidir.',
-          images: [
-            { src: '/static/img/courses-temp/tssf-back-1.png', alt: 'TSSF/CMAS 1* Dalıcı Sertifikası' },
-            { src: '/static/img/courses-temp/protec-org-1-2star.jpeg', alt: 'ProTec 1* Dalıcı Sertifikası' }
-          ],
-          hasPadiClasses: true,
-          prerequisites: [
-            '14 yaşını doldurmuş olmak',
-            '18 yaşının altındakiler için veli onayı',
-            'Yeterli seviyede yüzme bilmek',
-            'Dalış yapmanıza engel bir sağlık sorununuzun bulunmaması'
-          ],
-          includes: [
-            '8 saatlik teorik eğitim',
-            '5 açık deniz dalışı',
-            'Dalışlarda kullanılacak tüm ekipmanlar',
-            'Eğitim materyalleri',
-            'Logbook',
-            'Sertifika ücreti',
-            'Eğitmen giderleri',
-            'Eğitim boyunca çekilen tüm sualtı fotoğrafları'
-          ],
-          quota: 'Teorik eğitimler max 8:1, pratik eğitimler ise 4:1 verilmektedir. Sınıfın açılması için min 2 katılımcı gerekmektedir.',
-          alternativeCourses: []
-        },
-        // 2* Dalıcı Kursu
-        {
-          title: '2* Dalıcı Kursu',
-          price: 'Kurs ücreti 250€´dur.',
-          scope: 'Kursun amacı yeterli temel becerilere sahip dalıcının kurtarma, derin dalış, gece dalışı becerilerini arttırmaktır. Kurs sonunda isteğe bağlı olarak TSSF/CMAS veya ProTec belgesi alabilirsiniz. Alınan belgeler tüm dünyada geçerlidir.',
-          images: [
-            { src: '/static/img/courses-temp/tssf-back-2.png', alt: 'TSSF/CMAS 2* Dalıcı Sertifikası' },
-            { src: '/static/img/courses-temp/protec-org-1-2star.jpeg', alt: 'ProTec 2* Dalıcı Sertifikası' }
-          ],
-          hasPadiClasses: true,
-          prerequisites: [
-            'TSSF/CMAS için 15 yaşını doldurmuş olmak',
-            'ProTec için 16 yaşını doldurmuş olmak',
-            '18 yaşının altındakiler için veli onayı',
-            '1* Dalıcı (yada dengi) belgeye sahip olmak',
-            'TSSF/CMAS için 1* Dalıcı belgesini aldıktan sonra 20 kayıtlı dalış yapmış olmak',
-            'ProTec için 1* Dalıcı belgesini aldıktan sonra 25 kayıtlı dalış yapmış olmak',
-            'Dalış yapmanıza engel bir sağlık sorununuzun bulunmaması'
-          ],
-          includes: [
-            '8 saatlik teorik eğitim',
-            '5 açık deniz dalışı (1 derin dalış [42m], 1 gece dalışı)',
-            'Dalışlarda kullanılacak ekipmanlar',
-            'Eğitim materyalleri',
-            'Sertifika ücreti',
-            'Eğitmen giderleri',
-            'Eğitim boyunca çekilen tüm sualtı fotoğrafları'
-          ],
-          quota: 'Teorik eğitimler max 8:1, pratik eğitimler ise 4:1 verilmektedir. Sınıfın açılması için min 2 katılımcı gerekmektedir.',
-          alternativeCourses: []
-        },
-        // 3* Dalıcı Kursu
-        {
-          title: '3* Dalıcı Kursu',
-          price: 'Kurs ücreti 250€´dur.',
-          scope: 'Kursun amacı profesyonelliğe ilk adımı atmak isteyen dalıcı adaylarının liderlik becerilerini artıtmaktır. Kurs sonunda isteğe bağlı olarak TSSF/CMAS veya ProTec belgesi alabilirsiniz. Alınan belgeler tüm dünyada geçerlidir.',
-          images: [
-            { src: '/static/img/courses-temp/tssf-back-3.png', alt: 'TSSF/CMAS 3* Dalıcı Sertifikası' },
-            { src: '/static/img/courses-temp/protec-org-3star.jpeg', alt: 'ProTec 3* Dalıcı Sertifikası' }
-          ],
-          hasPadiClasses: true,
-          prerequisites: [
-            '18 yaşını doldurmuş olmak',
-            '2* Dalıcı (yada dengi) bir belgeye sahip olmak',
-            'TSSF/CMAS için 2* Dalıcı belgesine sahip olduktan sonra 50 kayıtlı dalış yapmış olmak',
-            'ProTec için 2* Dalıcı belgesine sahip olduktan sonra 60 kayıtlı dalış yapmış olmak',
-            'İlköğretim diplomasına sahip olmak',
-            'Derin Dalış, Temel İlkyardım, Yön Bulma uzmalıklarına sahip olmak',
-            'Dalış yapmanıza engel bir sağlık probleminizin bulunmaması'
-          ],
-          includes: [
-            '8 saatlik teorik eğitim',
-            '4 açık deniz dalışı (1 derin dalış [42m])',
-            'Dalışlarda kullanılacak ekipmanlar',
-            'Eğitim materyalleri',
-            'Sertifika ücreti',
-            'Eğitmen giderleri',
-            'Eğitim boyunca çekilen tüm sualtı fotoğrafları (talep etmeniz gerekir)'
-          ],
-          quota: 'Teorik eğitimler max 8:1, pratik eğitimler ise 4:1 verilmektedir. Sınıfın açılması için min 2 katılımcı gerekmektedir.',
-          alternativeCourses: []
-        },
+        BirYildizDalici,
+        IkiYildizDalici,
+        UcYildizDalici,
         // Modifiye Dalıcı Kursları
         {
           title: 'Modifiye Dalıcı Programları',
