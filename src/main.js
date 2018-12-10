777e30f9c352d59adbb3e769eb54374760224e94
Nevcan Uludaş <nevcanuludas@gmail.com>
2018-12-10 13:00:39 +0300

    Fixed, complate form added.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import Vuelidate from 'vuelidate'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
+import translations from './resources/translations'
 import VueFinger from 'vue-finger'
 import 'bootstrap/dist/css/bootstrap.css'
 import AppMainMenu from './components/MainMenu/MainMenu'
@@ -14,6 +17,9 @@ import AppFooterMobileMessage from './components/FooterMobileMessage/FooterMobil
 import AppScheduledClasses from './components/ScheduledClasses/ScheduledClasses'
 import AppFaq from './components/Faq/Faq'
 
+Vue.use(VueI18n)
+Vue.use(Vuelidate)
+Vue.config.formApiUrl = process.env.FORM_API_URL
 Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueFinger)
@@ -26,10 +32,18 @@ Vue.component('app-footer-mobile-message', AppFooterMobileMessage)
 Vue.component('app-scheduled-classes', AppScheduledClasses)
 Vue.component('app-faq', AppFaq)
 
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: translations
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
+  render: h => h(App),
   components: { App, AppMainMenu, AppSidebar, AppFooter, AppFooterMobileMessage },
   template: '<App/>'
 })
---
+++ src/resources/translations.js
@@ -0,0 +1,60 @@
+export default {
+  tr: {
+    form: {
+      firstName: 'Ad',
+      lastName: 'Soyad',
+      email: 'E-posta',
+      terms: 'Şartları ve koşulları kabul ediyorum',
+      type: 'Üyelik tipini seçiniz',
+      additionalInfo: 'İlave bilgi',
+      submitted: 'Form gönderildi!',
+      sentInfo: 'Gönderdiğiniz bilgiler:',
+      return: 'Forma geri dön',
+      submit: 'Gönder',
+      submitting: 'Gönderiliyor',
+      charactersLeft: 'You have {charCount} character left. | You have {charCount} characters left.',
+      types: {
+        free: 'Ücretsiz deneme aboneliği',
+        starter: 'Başlangıç aboneliği (50 € / ay)',
+        enterprise: 'Kurumsal abonelik (250 € / ay)'
+      }
+    },
+    error: {
+      invalidFields: 'Aşağıdaki alanlar geçersiz veya eksik:',
+      general: 'Gönderme sırasında bir hata oluştu.',
+      generalMessage: 'Teknik sorunlar nedeniyle form gönderilemedi. Daha sonra tekrar deneyin.',
+      fieldRequired: '{field} gereklidir.',
+      fieldInvalid: '{field} geçersiz veya eksik.',
+      fieldMaxLength: '{field} maksimum karakter aşıldı.'
+    }
+  },
+  en: {
+    form: {
+      firstName: 'First name',
+      lastName: 'Last name',
+      email: 'Email',
+      terms: 'I accept the terms and conditions',
+      type: 'Select subscription type',
+      additionalInfo: 'Additional info',
+      submitted: 'The form is submitted!',
+      sentInfo: 'Here is the info you sent:',
+      return: 'Return to the form',
+      submit: 'Submit',
+      submitting: 'Submitting',
+      charactersLeft: 'You have {charCount} character left. | You have {charCount} characters left.',
+      types: {
+        free: 'Free trial subscription',
+        starter: 'Starter subscription (50 € / month)',
+        enterprise: 'Enterprise subscription (250 € / month)'
+      }
+    },
+    error: {
+      invalidFields: 'Following fields have an invalid or a missing value:',
+      general: 'An error happened during submit.',
+      generalMessage: 'Form sending failed due to technical problems. Try again later.',
+      fieldRequired: '{field} is required.',
+      fieldInvalid: '{field} is invalid or missing.',
+      fieldMaxLength: '{field} maximum characters exceeded.'
+    }
+  }
+}
