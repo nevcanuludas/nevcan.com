7976edd76ff8998ac3de3a3e56ab5146d7c986ea
Nevcan Uludaş <nevcanuludas@users.noreply.github.com>
2020-06-17 12:29:13 +0300

    Update Detail.vue

--- src/components/Courses/Detail.vue
+++ src/components/Courses/Detail.vue
@@ -44,7 +44,7 @@ export default {
       path: this.$route.params.id,
       isVisible: false,
       // EURTRY: null,
-      EURTRY: 6.18,
+      EURTRY: 7.70,
       courseTitle: '',
       coursePrice: 'Kurs ücreti belirtilmemiştir. Teklif almak için lütfen iletişime geçin.',
       courseNote: '',
