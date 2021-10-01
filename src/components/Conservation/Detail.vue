0a828b0f55996452afe5db92768676a5a58cc164
Nevcan Uludaş <nevcanuludas@gmail.com>
2021-10-01 21:31:38 +0300

    Fix, consertavion details

--- src/components/Conservation/Detail.vue
+++ src/components/Conservation/Detail.vue
@@ -9,58 +9,64 @@ export default {
       conservationTitle: '',
       conservationScope: '',
       conservationUrl: '',
+      conservationLink: '',
       // JS Dictionary - Associative Arrays in JavaScript
       dict: {
         'pb-marmara': 0,
         'yayakarsa': 1,
         'diver-safety-guardian': 2,
         'sualti-kultur-mirasinin-korunmasi': 3,
-        'suluin-magarasi': 4,
+        'karstdive-2020': 4,
         'deniz-kiyi-plaj-temizligi': 5,
         'saros-sualti-muzesi': 6,
-        'photogrammetry-dpv': 7
+        'photogrammetry': 7
       },
       // Conservations JSON
       conservations: [
         {
           title: 'Project Baseline: Marmara',
           scope: 'Marmara bölgesi boyunca su koşulları ve denizanası sayılarını takip ediyoruz. Bu sayede zaman içerisinde gidişatı daha iyi görebileceğiz. Saros körfezinde ve İzmit körfezinde kurduğumuz iki farklı istasyon verileri toplamaya başladı bile. Sizde farkındalık sahibi bir dalıcı olarak bu projede bize destek olabilirsiniz.',
-          url: 'https://www.youtube-nocookie.com/embed/33FXnjxEPVc'
+          url: 'https://www.youtube-nocookie.com/embed/33FXnjxEPVc',
+          link: 'https://gueturkey.com/pb-marmara/'
         },
         {
           title: 'Ya Yakarsa',
-          scope: '',
-          url: ''
+          scope: 'Deniz anası artışı son zamanlarda iklim ve besin ağı değişiklikliğinden dolayı artıyor. Yurttaş bilimine destek olarak siz de bu projeye destek olabilirsiniz.',
+          link: 'https://yayakarsa.org'
         },
         {
           title: 'DAN Dalış Bağışı',
           scope: 'Dalış sonrası dalıcılarda oluşan sessiz kabarcıkların doppler ölçümü ile dinlendikten sonra, insan vücudu üzerindeki etkilerinin incelenmesi için ortak bir veritabanında toplanmasına yönelik yapılan bir yurttaş bilimidir.',
-          url: 'https://www.youtube-nocookie.com/embed/iFxiGp8gNIY'
+          url: 'https://www.youtube-nocookie.com/embed/iFxiGp8gNIY',
+          link: 'https://www.diversafetyguardian.org'
         },
         {
           title: 'Sualtı Kültür Mirasının Korunması',
           scope: 'Tarihin şekillenmesine yardımcı oluyoruz. Bulduğumuz kültür miraslarını arkeologlar ile paylaşıyoruz.',
-          url: 'https://www.youtube-nocookie.com/embed/whhoMdZUcIs'
+          url: 'https://www.youtube-nocookie.com/embed/rYxcSH0rWzg',
+          link: 'http://underwaterculturalheritage.net'
         },
         {
-          title: 'Suluin Mağarası',
-          scope: '1998 yılında JJ ve ekibinin 120 metreye kadar keşfettiği, aysa kıtasının en derin mağarası olan suluin mağarasında araştırma dalaışlarına devam ediyoruz. Amacımız, deniz ile bağlantısını ve dibini bulabilmek.',
-          url: 'https://www.youtube-nocookie.com/embed/NjX3O-RN3-8'
+          title: 'KarstDive 2020',
+          scope: '1998 yılında JJ ve ekibinin 122 metreye kadar keşfettiği, Aysa Kıtasının en derin mağarası olan Suluin Mağarasında araştırma dalaışlarına devam ediyoruz. Amacımız, deniz ile bağlantısını ve dibini bulabilmek.',
+          url: 'https://www.youtube-nocookie.com/embed/NjX3O-RN3-8',
+          link: ''
         },
         {
           title: 'Deniz, Kıyı ve Plaj Temizliği',
-          scope: '',
-          url: 'https://www.youtube-nocookie.com/embed/CVf_vbrc0to'
+          link: 'https://www.wwf.org.tr/calismalarimiz/oceans/deniz_ky_temizlii/'
         },
         {
           title: 'Saros Sualtı Müzesi',
           scope: '',
-          url: 'https://www.youtube-nocookie.com/embed/5BS3LHkt9hY'
+          url: 'https://www.youtube-nocookie.com/embed/5BS3LHkt9hY',
+          link: 'http://www.estab.gov.tr/dunyanin-ilk-su-alti-tarih-muzesi-saros-korfezinde/'
         },
         {
           title: 'Photogrammetry Yöntemi ile 3D Modelleme',
           scope: '',
-          url: 'https://www.youtube-nocookie.com/embed/D0UCWftL2ZI'
+          url: 'https://www.youtube-nocookie.com/embed/D0UCWftL2ZI',
+          link: 'https://sketchfab.com/nevcanuludas/followers'
         }
       ]
     }
@@ -70,6 +76,7 @@ export default {
     this.conservationTitle = c.title
     this.conservationScope = c.scope
     this.conservationUrl = c.url
+    this.conservationLink = c.link
   }
 }
 </script>
