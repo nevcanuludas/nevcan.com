9074b66a04279d844f88667b5e8641011cb2c6fd
Nevcan Uludaş <nevcanuludas@gmail.com>
2021-12-24 23:20:28 +0300

    Fix, scheduled classes component

--- src/components/ScheduledClasses/ScheduledClasses.vue
+++ src/components/ScheduledClasses/ScheduledClasses.vue
@@ -1,38 +1,12 @@
 <template lang="pug" src="./ScheduledClasses.pug"></template>
 
-<script type="text/javascript" src="//localendar.com/js/PublishedResponsiveIframe.js"></script>
-
 <script>
-import ScheduledClassesJson from './ScheduledClasses-2021.json'
 
 export default {
   name: 'ScheduledClasses',
   data () {
     return {
-      sectionTitle: 'Programlanmış Eğitimler',
-      trips: ScheduledClassesJson
-    }
-  },
-  mounted: function () {
-    var currentDate = new Date()
-
-    for (var i = 0; i < this.trips.length; i++) {
-      var startDate = new Date(this.trips[i].startDate)
-      var status = this.trips[i].status
-
-      if (startDate < currentDate) {
-        console.log(this.trips[i].name + ' ' + this.trips[i].date + ' tarihinde yapılmıştır.')
-        this.trips.shift()
-        i = -1
-      } else {
-        if (status === 'Dolu') {
-          this.trips[i].class = 'card-outline-danger'
-        } else if (status === 'Beklemede') {
-          this.trips[i].class = 'card-outline-secondary'
-        } else {
-          this.trips[i].class = 'card-outline-success'
-        }
-      }
+      sectionTitle: 'Programlanmış Eğitimler'
     }
   }
 }
